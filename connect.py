41a9894ca7dc01523f0ca93ff03247b8e8b979f0
wang wanlu <146597120+wangwanlu09@users.noreply.github.com>
2024-03-11 16:59:32 +1300

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
-dbuser = "wanlu" # Your MySQL username - likely 'root'
-dbpass = "Ruby600396" # ---- PUT YOUR PASSWORD HERE ----
-dbhost = "wanlu.mysql.pythonanywhere-services.com" 
+dbuser = "" # Your MySQL username - likely 'root'
+dbpass = " # ---- PUT YOUR PASSWORD HERE ----
+dbhost = "" 
 dbport = "3306"
-dbname = "wanlu$motorkhana"
+dbname = ""
